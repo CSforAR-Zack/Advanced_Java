02f2ff9cc0a7efdaa4c23016698bbc4f0a02c155
Zackary <zspink@nea.k12.ar.us>
2023-06-06 14:10:30 -0500

    Updated

--- 9_API_NASA/Main.java
+++ 9_API_NASA/Main.java
@@ -5,34 +5,50 @@ public class Main{
 
     public static void main(String[] args) throws IOException {
         // Get the URL of the NASA Image for the Day API
-        URL url = new URL("https://api.nasa.gov/planetary/apod");
+        URL url = new URL("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY");
         // Open a connection to the URL
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        // Set the API key as a request property
-        con.setRequestProperty("api_key", "DEMO_KEY");
-        // Set the request method to GET
+
         con.setRequestMethod("GET");
 
         // Get the response code
         int responseCode = con.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
 
         
-        if (responseCode == 200) {
-            InputStream inputStream = con.getInputStream();
-            FileOutputStream fileOutputStream = new FileOutputStream("nasa_image.jpg");
+        // Parse JSON data from the response without third party libraries
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(con.getInputStream()));
 
-            byte[] buffer = new byte[1024];
-            int bytesRead = inputStream.read(buffer);
-            while (bytesRead >= 0) {
-                fileOutputStream.write(buffer, 0, bytesRead);
-                bytesRead = inputStream.read(buffer);
-            }
-            fileOutputStream.close();
-            inputStream.close();            
-        } else {
-            System.out.println("Error: " + responseCode);
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        // Read the response line by line
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
         }
-
+        // Close the connection
+        in.close();
         con.disconnect();
+
+        int start = content.indexOf("img_src");
+        int end = content.indexOf(".JPG") + 4;
+        
+        // Get the URL of the image
+        String imageUrlString = content.substring(start + 10, end);
+        System.out.println(imageUrlString);
+        URL imageUrl = new URL(imageUrlString);
+        
+        InputStream is = imageUrl.openStream();
+        OutputStream os = new FileOutputStream("image.jpg");
+        int bytesRead = 0;
+        byte[] buffer = new byte[2048];
+        // Read the image data
+        while ((bytesRead = is.read(buffer, 0, 2048)) != -1) {
+            os.write(buffer, 0, bytesRead);
+        }
+        // Close the streams
+        is.close();
+        os.close();
+
     }
 }
